821277e28f0a8f8f0967d199820659a0f82c2b08
Aysel Panahova 16022 <apanahova16022@ada.edu.az>
2025-02-06 11:18:34 +0400

    maven project

---
+++ src/main/java/az/edu/ada/wm2/App.java
@@ -0,0 +1,16 @@
+package az.edu.ada.wm2;
+
+/**
+ * Hello world!
+ */
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello, Maven!");
+    }
+    public String getGreeting() {
+        return "Hello, Maven!";
+    }
+    
+}
+
---
+++ src/test/java/az/edu/ada/wm2/AppTest.java
@@ -0,0 +1,78 @@
+package az.edu.ada.wm2;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue() {
+        assertTrue(true);
+    }
+    @Test
+
+public void testAppHasAGreeting() {
+
+App app = new App();
+
+assertNotNull("App should have a greeting", app.getGreeting());
+
+}
+
+
+
+@Test
+
+public void testGreetingMessage() {
+
+App app = new App();
+
+assertEquals("Hello, Maven!", app.getGreeting());
+
+}
+
+
+@Test
+
+public void testAddition() {
+
+int result = 2 + 2;
+
+assertEquals(4, result);
+
+}
+
+
+@Test
+
+public void testSubtraction() {
+
+int result = 5
+ 
+-
+ 
+3;
+
+assertEquals(2, result);
+
+}
+
+
+@Test
+
+public void testFailCase() {
+
+
+assertTrue(true);
+
+}
+}
